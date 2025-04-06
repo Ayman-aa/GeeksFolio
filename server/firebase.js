15085f9fc79816aaae50895994b84f5914f39436
Ayman Aamam <57199947+Ayman-aa@users.noreply.github.com>
2025-04-06 21:38:05 +0100

    Update firebase.js

--- server/firebase.js
+++ server/firebase.js
@@ -2,13 +2,13 @@ const { initializeApp } = require("firebase/app");
 const { getFirestore } = require("firebase/firestore");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB0W0EV4uYiQmCwOWYrR7snJm2bB6dEGyI",
-  authDomain: "geeksfolio.firebaseapp.com",
-  projectId: "geeksfolio",
-  storageBucket: "geeksfolio.firebasestorage.app",
-  messagingSenderId: "96690261091",
-  appId: "1:96690261091:web:45393df84470dec923fc56",
-  measurementId: "G-1NHMXXRSL2"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
